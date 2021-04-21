7dcdf7d5b995eba2f82a5433e9013f8f21b9d17d
aivanov <aivanov@sberhealth.ru>
2021-04-21 12:56:57 +0300

    21.04.2021

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import express from 'express';
+const app = express();
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3004);
+  await app.listen(process.env.PORT || 3004,() => console.log('Application is listening on port 3004'));
 }
 bootstrap();
